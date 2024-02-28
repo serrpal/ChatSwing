2346cd0d31d170340adcb790690ea1a9e3f0c4f2
Paula <paulaserranolopez_7826@escuelaces.net>
2024-02-28 20:14:31 +0100

    Estructura básica server

--- src/util/ChatServer.java
+++ src/util/ChatServer.java
@@ -14,12 +14,21 @@ public class ChatServer implements Runnable{
 
     private ServerSocket server;
     private boolean exit;
+    private int port = 12345;
+
     public ChatServer(){
         exit = false;
     }
 
     @Override
     public void run() {
-
+        try {
+            server = new ServerSocket(port);
+            while (!exit){
+                Socket client = server.accept();
+            }
+        }catch (IOException e){
+            //TODO
+        }
     }
 }
