89e04617003fdbcab9d01efca275ce3aa05bd66f
Paula <paulaserranolopez_7826@escuelaces.net>
2024-02-28 20:22:48 +0100

    Método broadcastMessage y adiciones a clase interna Handler

--- src/util/ChatServer.java
+++ src/util/ChatServer.java
@@ -4,6 +4,7 @@ import gui.Chat;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 
 public class ChatServer implements Runnable{
 
+    private ArrayList<Handler> clientsConnected;
     private ServerSocket server;
     private boolean exit;
     private int port = 12345;
@@ -44,7 +46,25 @@ public class ChatServer implements Runnable{
         }
         @Override
         public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                //Gets username from GUI
+                username = in.readLine();
+                //Shows nickname to all clients connected
+                broadcastMessage(username + " se unió al chat");
 
+            }catch (IOException e){
+                //TODO : handle exception
+            }
+        }
+
+        public void broadcastMessage(String message){
+            for (Handler ch : clientsConnected){
+                if (ch!=null){
+                    //TODO
+                }
+            }
         }
     }
 }
