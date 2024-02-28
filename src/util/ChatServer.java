9d65b10bdb450c4e36b9bf3abc52793d56b406fe
Paula <paulaserranolopez_7826@escuelaces.net>
2024-02-28 20:12:35 +0100

    Comienzo de cero lado server para realizar la corrección

--- src/util/ChatServer.java
+++ src/util/ChatServer.java
@@ -6,10 +6,17 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ChatServer implements Runnable{
+
+    private ServerSocket server;
+    private boolean exit;
+    public ChatServer(){
+        exit = false;
+    }
 
     @Override
     public void run() {
